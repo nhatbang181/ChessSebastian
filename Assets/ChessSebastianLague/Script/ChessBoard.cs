334b094fbeb409b2531ee3157f0dc34bc5a20895
Bang <nhatbang181@gmail.com>
2025-12-01 23:54:59 +0700

    feat: Implement initial chess board generation with materials, prefab, script, and scene setup.

--- Assets/ChessSebastianLague/Script/ChessBoard.cs
+++ Assets/ChessSebastianLague/Script/ChessBoard.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class ChessBoard : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+ [SerializeField] private Material whiteSquareMaterial;
+ [SerializeField] private Material blackSquareMaterial;
+ [SerializeField] private GameObject squarePrefab ;
+ [SerializeField] private Transform squareParent;
+ [SerializeField] private float squareSize = 1f;
+ void Start() {
+    CreateChessBoardGraphic();
+ }
+ public void CreateChessBoardGraphic() {
+    for (int file = 0; file < 8; file++) {
+        for (int rank = 0; rank < 8; rank++) {
+            GameObject square = Instantiate(squarePrefab, squareParent);
+            square.transform.localScale = new Vector3(1, 1, 1);
+            square.transform.localPosition = new Vector3(file * squareSize, 0, rank * squareSize)  ;
+            square.transform.localRotation = Quaternion.identity;
+            square.GetComponentInChildren<Renderer>().material = (file + rank) % 2 == 0 ? whiteSquareMaterial : blackSquareMaterial;
+            square.name = $"{file}{rank}";
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ }
 }
